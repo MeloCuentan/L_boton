66f9c27bfd6677a77a2835faaf77c83b7ebca32e
Luiti_LP <89021748+MeloCuentan@users.noreply.github.com>
2023-07-24 15:49:48 +0100

    Update L_boton.cpp

--- src/L_boton.cpp
+++ src/L_boton.cpp
@@ -18,6 +18,13 @@ L_boton::L_boton(uint8_t _pin, uint16_t _TIEMPO_PULSO_LARGO) {
   pinMode(pin, INPUT_PULLUP);
 }
 
+L_boton::L_boton(uint8_t _pin, uint16_t _TIEMPO_PULSO_CORTO, uint16_t _TIEMPO_PULSO_CORTO) {
+  pin = _pin;
+  TIEMPO_PULSO_CORTO = _TIEMPO_PULSO_CORTO;
+  TIEMPO_PULSO_LARGO = _TIEMPO_PULSO_LARGO;
+  pinMode(pin, INPUT_PULLUP);
+}
+
 
 // Leemos el estado del pin y si es pulso corto devolvemos 1, 
 // si es largo 2 y si no se ha pulsado 0 
